3f8af90ce429beea62ded5781e4b8c0b9db076ff
GrantHaines <33074572+GrantHaines@users.noreply.github.com>
2021-02-10 19:53:01 -0800

    Switch to new site

--- README.md
+++
@@ -1,3 +0,0 @@
-# GrantHaines.github.io
-
-This is the preliminary project where I am working on my personal website. Nothing here is final: I will most likely start using a CMS when I actually want to do it. Right now I'm just messing around and practicing my web dev skills.
---
+++ projects/sunsetcoc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php $thisPage="Project - Sunset CoC Website" ?>
+
+<head>
+	<?php include("../phpincludes/header.php"); ?>
+	<link rel="stylesheet" type="text/css" href="/~hainesg/css/project.css">
+</head>
+
+<body>
+	<?php include("../phpincludes/topbar.php"); ?>
+	<div id="contentBox">
+		<p id="backLink"><a href="http://web.engr.oregonstate.edu/~hainesg">Back to Main Page</a></p>
+		<h1 class="projectTitle">Sunset Church of Christ Website</h1>
+		<img class="image" src="/~hainesg/images/church3.png"></img>
+		<p class="descriptionText">This website is a replacement for a static HTML/CSS website I previously created in 2018.</p>
+	</div>
+	<?php include ("../phpincludes/footer.php"); ?>
+</body>
+
+</html>
+<script>
+</script>
